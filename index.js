f03770dbc31ce472b57bf2306d9c498d2c46277e
Zorkonn <zorkon2002@gmail.com>
2018-05-03 20:04:25 +0300

    Added Bot information

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const botconfig = require("./botconfig.json");
 const tokenfile = require("./token.json");
 const Discord = require('discord.js');
 
+let cooldown = new Set();
+let cdseconds = 120;
+
 const bot = new Discord.Client({
     disableEveryone: true
 });
@@ -14,12 +17,21 @@ bot.on("ready", async () => {
 
 bot.on("message", async message => {
     if(message.author.bot) return;
-    if(message.channel.type === dm) return;
+    if(message.channel.type === "dm") return;
 
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
+
+    if(cmd === `Test`){
+        let botembed = new Discord.RichEmbed()
+            .setDescription("**Bot bilgisi**")
+            .setColor("#2d845b")
+            .addField(bot.user.username, "Sığınağa yürüme uzaklığı (Ping):  " + "***" + bot.ping + "ms" + "***")
+            .setThumbnail(bot.user.displayAvatarURL)
+            return message.channel.send(botembed);
+    }
 });
 
 
-bot.login(botconfig.token);
+bot.login(tokenfile.token);
